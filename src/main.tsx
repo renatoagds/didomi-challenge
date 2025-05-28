cb5a45cbb6e0a08ca53bc7d8ca5197f3162d5f7a
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2025-05-28 12:11:22 +0200

    feat: add react-router on declarative mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -11,6 +12,8 @@ import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
