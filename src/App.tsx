86c41277a42074f4b6f97469ead8dd546de237ff
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2025-05-28 12:35:40 +0200

    feat: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+/* Pages */
+import GiveConsent from "./pages/GiveConsent";
+import CollectedConsents from "./pages/CollectedConsents";
+import { Link, Navigate, Route, Routes } from "react-router";
+
+const drawerWidth = 240;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <Box sx={{ display: "flex" }}>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Divider />
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/give-consent">
+                <ListItemText primary="Give consent" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/collected-consents">
+                <ListItemText primary="Collected consents" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Routes>
+            <Route index path="/" element={<Navigate to="/give-consent" />} />
+            <Route path="/give-consent" element={<GiveConsent />} />
+            <Route path="/collected-consents" element={<CollectedConsents />} />
+          </Routes>
+        </Box>
+      </Box>
     </>
   );
 }
---
+++ src/pages/CollectedConsents.tsx
@@ -0,0 +1,3 @@
+export default function CollectedConsents() {
+  return <div>CollectedConsents</div>;
+}
