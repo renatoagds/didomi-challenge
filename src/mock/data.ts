06a9feb5b72f038bab10a43c3b1a2d7bc54884ff
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2025-05-29 12:30:23 +0200

    feat: add mock data

---
+++ src/mock/data.ts
@@ -0,0 +1,74 @@
+export type ConsentData = {
+  id: string;
+  name: string;
+  email: string;
+  consent: {
+    newsletter: boolean;
+    ads: boolean;
+    contribute: boolean;
+  };
+};
+
+const data: Array<ConsentData> = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    consent: {
+      newsletter: true,
+      ads: false,
+      contribute: true,
+    },
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    consent: {
+      newsletter: false,
+      ads: true,
+      contribute: false,
+    },
+  },
+  {
+    id: "3",
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com",
+    consent: {
+      newsletter: true,
+      ads: true,
+      contribute: false,
+    },
+  },
+  {
+    id: "4",
+    name: "Bob Brown",
+    email: "bob.brown@example.com",
+    consent: {
+      newsletter: false,
+      ads: false,
+      contribute: true,
+    },
+  },
+  {
+    id: "5",
+    name: "Charlie White",
+    email: "charlie.white@example.com",
+    consent: {
+      newsletter: true,
+      ads: false,
+      contribute: true,
+    },
+  },
+];
+
+export function getData(): Array<ConsentData> {
+  return data;
+}
+
+export function addData(newEntry: Omit<ConsentData, "id">): Array<ConsentData> {
+  const newId = (data.length + 1).toString();
+  const entryWithId = { ...newEntry, id: newId };
+  data.push(entryWithId);
+  return data;
+}
---
+++ src/mock/mock.ts
@@ -0,0 +1,37 @@
+import { addData, getData, type ConsentData } from "./data";
+
+window.fetch = async (
+  input: RequestInfo | URL,
+  init?: RequestInit
+): Promise<Response> => {
+  const url =
+    typeof input === "string"
+      ? input
+      : input instanceof URL
+      ? input.toString()
+      : input.url;
+
+  const method = init?.method || "GET";
+
+  if (url.includes("/consents") && method === "GET") {
+    return new Response(JSON.stringify({ data: getData() }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (url.includes("/consents") && method === "POST") {
+    const parsedBody: Omit<ConsentData, "id"> = init?.body
+      ? JSON.parse(init.body as string)
+      : {};
+
+    const updatedData = addData(parsedBody);
+
+    return new Response(JSON.stringify(updatedData), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return new Response(null, { status: 404 });
+};
